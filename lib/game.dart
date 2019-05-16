a5703c4c5f07569a41b9bcc1a6701ce88fa6db04
ChacaS0 <36397520+ChacaS0@users.noreply.github.com>
2019-05-16 09:17:20 +0400

    Dev/ci cd (#4)
    * travis ci
    
    * :wrench: CI§CD
    
    * :abc: Flutter format document
    
    * :pencil: :wrench: edit .travis.yml
    
    * :scroll: travis_script
    
    * Update dependencies packages for travis
    
    * Update dependencies packages for travis
    
    * :heavy_check_mark: :wrench: ci/cd

--- lib/game.dart
+++ lib/game.dart
@@ -1,27 +1,21 @@
-
 import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sensors/sensors.dart';
 
-
-class Game extends StatefulWidget  {
+class Game extends StatefulWidget {
   @override
   _FlutterGameState createState() => _FlutterGameState();
 }
 
-class _FlutterGameState extends State<Game> with SingleTickerProviderStateMixin {
-  
-
+class _FlutterGameState extends State<Game>
+    with SingleTickerProviderStateMixin {
   int _shakeCounter;
   bool _isTime = false;
 
   @override
   Widget build(BuildContext context) {
-
-   
-
     return _buildGameScaffold();
   }
 
@@ -32,96 +26,74 @@ class _FlutterGameState extends State<Game> with SingleTickerProviderStateMixin 
     super.dispose();
   }
 
-  void _listenShakes(){ _shakeCounter = 0;
+  void _listenShakes() {
+    _shakeCounter = 0;
 
     double _prevY = 1;
     bool _isRising = false;
 
     accelerometerEvents.listen((AccelerometerEvent event) {
       // print(event.y);
-      if((event.y - _prevY).abs() > 0.2 && _isTime) {
-        
-        if(event.y > _prevY) {
+      if ((event.y - _prevY).abs() > 0.2 && _isTime) {
+        if (event.y > _prevY) {
           // it's going up
-          if(!_isRising) {
+          if (!_isRising) {
             _shakeCounter++;
           }
           _isRising = true;
-        } else if (event.y < _prevY ) {
+        } else if (event.y < _prevY) {
           // i's going down
-          if(_isRising) {
+          if (_isRising) {
             _shakeCounter++;
           }
           _isRising = false;
         }
-        
+
         _prevY = event.y;
 
         print(_shakeCounter);
       }
-    });}
-
-  Scaffold _buildGameScaffold() {
-
-    return new Scaffold(
-      appBar: AppBar(title: Text("Timer test")),
-      body: Center(
-        child : Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          RaisedButton(
-            onPressed: () {
-              startTimer();
-              _listenShakes();
-            },
-            
-            child: Text("start"),
-          ),
-          Text("$_start")
-        ],
-      )));
+    });
   }
 
-Timer _timer;
-int _start =10;
+  Scaffold _buildGameScaffold() {
+    return new Scaffold(
+        appBar: AppBar(title: Text("Timer test")),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            !_isTime
+                ? RaisedButton(
+                    onPressed: () {
+                      _start = 10;
+                      startTimer();
+                      _listenShakes();
+                    },
+                    child: Text("Start"),
+                  )
+                : Text("Shake it !"),
+            Text("$_start")
+          ],
+        )));
+  }
 
-void startTimer() {
-  
+  Timer _timer;
+  int _start = 10;
 
-  const oneSec = const Duration(seconds: 1);
-  _isTime = true;
-  _timer = new Timer.periodic(
-      oneSec,
-      (Timer timer) => setState(() {
-            if (_start < 1) {
-              timer.cancel();
-              _isTime = false;
-            } else {
-              _start = _start - 1;
-            }
-          }));
+  void startTimer() {
+    const oneSec = const Duration(seconds: 1);
+    _isTime = true;
+    _timer = new Timer.periodic(
+        oneSec,
+        (Timer timer) => setState(() {
+              if (_start < 1) {
+                timer.cancel();
+                _isTime = false;
+              } else {
+                _start = _start - 1;
+              }
+            }));
+  }
 }
-
-Timer _timer;
-int _start =10;
-
-void startTimer() {
-  
-  const oneSec = const Duration(seconds: 1);
-  _isTime = true;
-  _timer = new Timer.periodic(
-      oneSec,
-      (Timer timer) => setState(() {
-            if (_start < 1) {
-              timer.cancel();
-              _isTime = false;
-            } else {
-              _start = _start - 1;
-            }
-          }));
-}
-
-
-}
-
